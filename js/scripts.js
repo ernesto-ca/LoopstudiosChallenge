d5d39a05f70c4102d4046f4e6dc27d5358a7207e
Ernesto Cabañas <71567859+ernesto-ca@users.noreply.github.com>
2021-01-23 19:17:26 -0600

    Completely finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 /* Open */
 function openNav() {
     document.getElementById("menu").style.height = "100%";
@@ -7,3 +8,4 @@ function openNav() {
 function closeNav() {
     document.getElementById("menu").style.height = "0%";
 } 
+
